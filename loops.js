7b98e62ee53a11c5bba53dbf9f312b29b913c827
niranjandas25 <49498020+niranjandas25@users.noreply.github.com>
2019-12-25 19:54:02 +0530

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,34 @@
+
+//for loop
+//Summation upto 100
+
+var sum = 0;
+for (let i=0; i<=100; i++)
+{ 
+	
+	sum = sum+i;
+}
+console.log("The sum using for loop is: " + sum)
+
+//While loop
+//Summation upto 100
+var i=0
+var sum = 0;
+while(i<=100)
+{
+	sum = sum+i;
+	i++;
+}
+console.log("The sum using while is: " + sum)
+
+//do While loop
+//Summation upto 100
+var i=0;
+var sum = 0;
+do
+{
+	sum = sum+i;
+	i++;
+}while(i<=100);
+console.log("The sum using do while is: " + sum)
+
